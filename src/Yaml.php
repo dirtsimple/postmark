8befce8a9261b26285bddab378b4025d90fa2a87
PJ Eby <pje@telecommunity.com>
2019-09-06 14:47:39 -0400

    Streamline and document the YAML dump algorithm

--- src/Yaml.php
+++ src/Yaml.php
@@ -1,25 +1,23 @@
 <?php
 namespace dirtsimple\Postmark;
 
-use Mustangostang\Spyc;
 use Symfony\Component\Yaml\Yaml as Yaml12;
 use Symfony\Component\Yaml\Tag\TaggedValue;
 use Symfony\Component\Yaml\Inline;
 use Symfony\Component\Yaml\Exception\DumpException;
 
 class Yaml {
+	const
+		ROOT_FLAGS = Yaml12::DUMP_OBJECT_AS_MAP,
+		DUMP_FLAGS = Yaml12::DUMP_OBJECT_AS_MAP |
+		             Yaml12::DUMP_EMPTY_ARRAY_AS_SEQUENCE;
 
 	static function parse($data, $filename=null) {
 		return Yaml12::parse($data);
 	}
 
 	static function dump($data) {
-		return static::_dump(
-			$data,
-			Yaml12::DUMP_OBJECT_AS_MAP |
-			Yaml12::DUMP_MULTI_LINE_LITERAL_BLOCK |
-			Yaml12::DUMP_EMPTY_ARRAY_AS_SEQUENCE
-		);
+		return static::_dump($data);
 	}
 
 	static function parseFile($filename) {
@@ -28,74 +26,66 @@ class Yaml {
 
 	# Like Symfony's dump, but w/fixes for block chomping on multiline literals,
 	# and nested data structures are only inlined if they can fit on the current
-	# line without wrapping, and have no non-empty containers as children
+	# line without wrapping, and have no non-empty containers as children.
 	#
-	protected static function _dump($data, $flags, $tabsize=2, $width=120, $indent=0, $keylen=0) {
-		$prefix = str_repeat(' ', $indent);
-
-		if ( static::_is_leaf($data) ) {
-			if ($keylen) {
+	# The overall goal is to optimize for revision control, such that diffs
+	# don't contain extraneous changes, and line breaks follow the source data
+    # (allowing in-line change highlighting where tooling allows).  For this
+    # reason, we don't do line folding, even though it would improve readability,
+    # because even a small change to such a string could rewrap the rest of the
+    # string, polluting the diff with semantically-irrelevant changes.
+	#
+	protected static function _dump($data, $width=120, $indent='  ', $prefix='', $keylen=null) {
+		# See if $data can be rendered as a simple (leaf) value
+		switch(true) {
+		case is_string($data):
+			if (
+				( $lines = substr_count($data, "\n") ) && # multi-line
+				false === strpos($data, "\r\n") &&        # no lossy CRLFs
+				($lines > 1 || strlen($data) > $width - $keylen) # two LFs or too wide
+			) {
+				$indicator = substr_compare($data, ' ', 0, 1) ? '': strlen($indent);
+				if ( substr_compare($data, "\n", -1) ) {
+					$data .= "\n";
+					$indicator .= '-';  # strip the \n we added
+				} else if ( ! substr_compare($data, "\n\n", -2) ) {
+					$indicator .= '+';  # keep trailing blank lines
+				}
+				return "|$indicator\n" . preg_replace('/^/m', "$prefix$indent", $data);
+			}
+			# ...else intentional fallthrough, since strings are leaves
+		case is_scalar($data):	# inline other common leaf cases and fall through
+		case empty($data):
+		case static::_is_leaf($data):
+			if ( isset($keylen) ) {
 				# Recursive call: return everything on one line
-				return Inline::dump($data, $flags);
+				return Inline::dump($data, self::DUMP_FLAGS);
 			} else {
 				# Root call: include prefix and trailing LF
-				return $prefix . Inline::dump($data, $flags & ~Yaml12::DUMP_EMPTY_ARRAY_AS_SEQUENCE) . "\n";
+				return $prefix . Inline::dump($data, self::ROOT_FLAGS) . "\n";
 			}
 		}
 
-		$out = array('');
+		# Not a leaf, it's a non-empty array (or array-like object)
+		$out = array();
 		$isMap = Inline::isHash($data);
-		$can_inline = true;
-		$inline_room = $width - $keylen - 4;  # allow for [ ] / { }
-		$memo = array();
-
+		$room = $width - $keylen;
+		$width -= strlen($indent);
+		$nested = "$prefix$indent";
 		foreach ($data as $k => $v) {
-			$can_inline = ( $inline_room > 0 ) && static::_is_leaf($v);
-			if ( ! $can_inline ) break;
-			$val = Inline::dump($v, $flags);
-			$val = $memo[$k] = $isMap ? Inline::dump($k) . ": $val" : $val;
-			$inline_room -= strlen($val) + 2;
+			$k = $isMap ? Inline::dump($k, self::DUMP_FLAGS).': ' : '';
+			$v = static::_dump($v, $width, $indent, $nested, strlen($k));
+			$out[] = $v = "$k$v";
+			$room -= substr_compare($v, "\n", -1) ? strlen($v) + 2 : $room;
 		}
 
-		if ( $can_inline ) {
-			return sprintf( $isMap ? "{ %s }" : "[ %s ]", implode(', ', $memo) );
+		if ( $room >= 4 ) {  # allow for [ ] / { }
+			return sprintf( $isMap ? "{ %s }" : "[ %s ]", implode(', ', $out) );
 		}
 
-		foreach ($data as $k => $v) {
-			if ( is_string($v) && strpos($v, "\n") !== false ) {
-				# Could this key's value be rendered as a multi-line literal?
-				$n = count( $lines = explode("\n", $v) );
-				if ( $n > 1 && false === strpos($v, "\r\n") ) {
-					$indicator = (' ' === substr($v, 0, 1)) ? $tabsize : '';
-					if ( $lines[$n -1] !== '' ) {
-						$indicator .= '-';  # strip trailing \n
-					} else if ( $lines[$n-2] === '' ) {
-						$indicator .= '+';  # keep trailing blank lines
-					} else array_pop($lines); # drop unneeded blank line
-					$k = $isMap ? Inline::dump($k, $flags).':' : '-';
-					$out[] = "$k |$indicator\n";
-					$pre = str_repeat(' ', $tabsize);
-					foreach ( $lines as $line ) $out[] = "$pre$line\n";
-					continue;
-				}
-			}
-
-			# Not a multi-line literal; can we re-use an existing entry?
-			if ( array_key_exists($k, $memo) ) {
-				$out[] = ( $isMap ? "": "- " ) . $memo[$k] . "\n";
-				continue;
-			}
-
-			# Not memoized or a multi-line literal; recurse:
-			$k = $isMap ? Inline::dump($k, $flags).':' : '-';
-			$v = static::_dump($v, $flags, $tabsize, $width, $indent+$tabsize, strlen($k)+1);
-			if ( substr($v, -1) !== "\n" ) {
-				$out[] = "$k $v\n";
-			} else {
-				$out[] = "$k\n$v";
-			}
-		}
-		return implode($prefix, $out);
+		$out = preg_replace('/([^\n])$/D', "\\1\n", $out);  # add missing LFs
+		if ( ! $isMap ) $prefix .= '- ';
+		return (isset($keylen) ? "\n" : "") . $prefix . implode($prefix, $out);
 	}
 
 	protected static function _is_leaf($data) {
